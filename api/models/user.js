510c1318e5eb11799eba58704b845de4ca399101
BenSpankEm <39414280+Benspankem@users.noreply.github.com>
2019-09-09 10:39:58 -0700

    Add files via upload

---
+++ api/models/user.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    emailAddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: 'Email address is already in use',
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  }, {});
+  User.associate = function(models) {
+    User.hasMany(models.Course, {
+      foreignKey: {
+        fieldName: "userId",
+        allowNull: false,
+      }
+    });
+  };
+  return User;
+};
